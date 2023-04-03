c224b5f57d1198a8845ab58e402413bc95806ad0
Diddyy <joshua.allen@outlook.com>
2023-04-03 15:17:53 +0100

    Formatting

--- Communication/Packets/Incoming/RP/Inventory/UpdateRPItemPositionEvent.cs
+++ Communication/Packets/Incoming/RP/Inventory/UpdateRPItemPositionEvent.cs
@@ -7,10 +7,8 @@ internal class UpdateRPItemPositionEvent : IPacketEvent
 {
     public async Task Parse(GameClient session, IIncomingPacket packet)
     {
-        Console.WriteLine("UpdateRPItemPositionEvent");
         int itemId = packet.ReadInt();
         int position = packet.ReadInt();
-
 
         await session.GetHabbo().RPInventory.UpdateItemPosition(itemId, position);
         session.Send(new UpdateRPItemPositionComposer(itemId, position));
